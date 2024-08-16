21dab54e86badb1dde01ccf4740faab3067d8ec7
Ben <wilsonben@rogers.com>
2024-08-16 01:01:59 -0400

    new-version

--- main.js
+++ main.js
@@ -1,61 +1,173 @@
-function renderList(list){
-	if(list.length <= 0){
-		return
-	}
+const charName = document.getElementById("name");
+const charRoll = document.getElementById("roll");
+const addChar = document.getElementById("add-char");
+const charList = document.getElementById("char-list");
+const inputForm = document.getElementById("input-form");
+const clearBtn = document.getElementById("clear-btn");
 
-	const charlist = document.getElementById('chars');
-	const copylist = document.getElementById('copy');
-	let copy = list[0].name;
-	list.slice(1).map(c => copy += (" - " + c.name)).join('');
-	charlist.innerHTML = list.map(c => '<li><pre>' + c.name + " &emsp; Roll: " + c.roll + '</pre><button class="btn">Delete</button></li>').join('');
-	
+const charData = JSON.parse(localStorage.getItem("data")) || [];
+const charCopyList = document.getElementById("copy-list");
+const currentChar = {};
 
-	const deleteBtn = document.getElementsByClassName("btn");
-	Array.prototype.slice.call(deleteBtn).forEach(function(item) {
-		item.addEventListener("click", function(e) {
-			var els = e.target.parentNode.parentNode.getElementsByTagName('li');
-			var i = 0;
-			while(!(els[i] === e.target.parentNode) && i < list.length){
-				i++;
-			}
-			list.splice(i,1);
-			e.target.parentNode.remove()	
-			if(list.length > 0){
-				copy = list[0].name;
-				list.slice(1).map(c => copy += (" - " + c.name)).join('');
-				copylist.innerHTML = copy;
-			}else{
-				copy = "";
-				copylist.innerHTML = "";
-			}
-			sessionStorage.setItem("list", JSON.stringify(list));
-		});
-	})
-	copylist.innerHTML = copy;
+
+const addOrUpdateCharList = () => {
+    const dataArrIndex = charData.findIndex((char) => char.id === currentChar.id);
+    const charObj = {
+        id: `${charName.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        name: charName.value,
+        roll: charRoll.value,
+        status: []
+    };
+
+    if (dataArrIndex === -1) {
+        charData.unshift(charObj);
+    } else {
+         charData[dataArrIndex] = charObj;
+    }
+
+    localStorage.setItem("data", JSON.stringify(charData));
+    update();
+    reset();
 }
 
-window.addEventListener('load', () => {
-	const form = document.querySelector("#new-char-form");
-	const charinput = document.querySelector("#new-char-input");
-	const rollinput = document.querySelector("#new-roll-input");
-	const list_el = document.querySelector("#chars");
-	const list = JSON.parse(sessionStorage.getItem('list')) || [];
-	renderList(list);
+const updateCopyList= () => {
+    let list = "";
+    charData.forEach(
+      ({ name }) => {
+          list += name + " - ";
+      }
+    );
+    list = list.slice(0, -3);
+    charCopyList.innerText = list;
+  };
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const roll = parseFloat(rollinput.value);
-		const name = charinput.value;
-		if(isNaN(roll) || name == ""){
-			alert("Invalid Roll Value");
-		}else{
-			const char = {name: name, roll: roll};
-			list.push(char);
-			list.sort((a, b) => b.roll - a.roll);
-			renderList(list);
-			rollinput.value = '';
-			charinput.value = '';
-			sessionStorage.setItem("list", JSON.stringify(list));
-	 	}
-	});
+const fillStatusList = () => {
+    const lists = document.getElementsByClassName("status-list");
+    Array.from(lists).forEach( (list) => {
+        const dataArrIndex = charData.findIndex(
+            (char) => char.id === list.parentElement.parentElement.id
+        );
+
+        charData[dataArrIndex].status.forEach((status) => {
+            list.innerHTML += `<div onclick="deleteStatus(this)" class="status-card">${status}</div>`
+        });
+    });
+}
+
+const updateCharContainer = () => {
+    charList.innerHTML = "";
+    charData.forEach(
+      ({ id, name, roll }) => {
+          (charList.innerHTML += `
+          <div class="char" id="${id}">
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Roll:</strong> ${roll}</p>
+            <div class="status-container" id="stat-${id}">
+            <select name="status" class="status">
+                <option value="">--Status--</option>
+                <option value="Blinded">Blinded</option>
+                <option value="Charmed">Charmed</option>
+                <option value="Deafened">Deafened</option>
+                <option value="Frightened">Frightened</option>
+                <option value="Grappled">Grappled</option>
+                <option value="Incapacitated">Incapacitated</option>
+                <option value="Invisible">Invisible</option>
+                <option value="Paralyzed">Paralyzed</option>
+                <option value="Petrified">Petrified</option>
+                <option value="Poisoned">Poisoned</option>
+                <option value="Prone">Prone</option>
+                <option value="Restrained">Restrained</option>
+                <option value="Stunned">Stunned</option>
+                <option value="Unconscious">Unconscious</option>
+                <option value="Concentration">Concentration</option>
+            </select>
+            <button class="status-btn" onclick="select(this)" type="button">Add Status</button>
+            <div class="status-list"></div>
+            </div>
+            <button class="btn" onclick="deleteChar(this)" type="button" class="btn">Delete</button> 
+          </div>
+        `)
+      }
+    );
+  };
+
+
+const deleteChar = (buttonEl) => {
+    const dataArrIndex = charData.findIndex(
+        (char) => char.id === buttonEl.parentElement.id
+    );
+
+    buttonEl.parentElement.remove();
+    charData.splice(dataArrIndex, 1);
+    update();
+    localStorage.setItem("data", JSON.stringify(charData));
+}
+
+const deleteStatus = (statEl) => {
+    const charEl = statEl.parentElement.parentElement.parentElement;
+    const status = statEl.innerText;
+    const dataArrIndex = charData.findIndex(
+        (char) => char.id === charEl.id
+    );
+
+    const statusIndex = charData[dataArrIndex].status.findIndex(
+        (stat) => stat === status
+    );
+
+    charData[dataArrIndex].status.splice(statusIndex, 1);
+    statEl.remove();
+    localStorage.setItem("data", JSON.stringify(charData));
+    update();
+}
+
+const select = (buttonEl) => {
+    const selectedStatus = buttonEl.parentElement.children[0].value;
+    const dataArrIndex = charData.findIndex(
+        (char) => char.id === buttonEl.parentElement.parentElement.id
+    );
+
+    if(selectedStatus && !charData[dataArrIndex].status.includes(selectedStatus)){
+        charData[dataArrIndex].status.push(selectedStatus)
+        buttonEl.parentElement.children[2].innerHTML += `<div onclick="deleteStatus(this)" class="status-card">${selectedStatus}</div>`;
+    }
+    localStorage.setItem("data", JSON.stringify(charData));
+}
+  
+
+const reset = () => {
+    charName.value = "";
+    charRoll.value = "";
+    currentTask = {};
+}
+
+
+const sortCharList = () => {
+    charData.sort(function(a, b) { 
+        return Number(b.roll) - Number(a.roll);
+    })
+}
+
+inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addOrUpdateCharList();
 });
+
+clearBtn.addEventListener("click", () => {
+    localStorage.clear();
+    charData.splice(0, charData.length);
+    localStorage.setItem("data", JSON.stringify(charData));
+    update();
+    reset()
+});
+
+const update = () =>{
+    sortCharList();
+    updateCharContainer()
+    updateCopyList();
+    fillStatusList();
+}
+
+
+if (charData.length) {
+    update();
+}
