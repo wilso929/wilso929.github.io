6b1544c0d7d2f44fae0b68011cb4bee00e3dd5a7
wilso929 <94696454+wilso929@users.noreply.github.com>
2024-09-07 13:59:27 -0400

    custom-status (#6)
    Co-authored-by: Ben <wilsonben@rogers.com>

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ const charList = document.getElementById("char-list");
 const inputForm = document.getElementById("input-form");
 const clearBtn = document.getElementById("clear-btn");
 
+const closeSpell = document.getElementById("close-spell");
+const closeCustom = document.getElementById("close-custom");
+
+const concentrationForm = document.getElementById("concentration-form");
+const customForm = document.getElementById("custom-form");
+const concentrationSpell = document.getElementById("spell");
+const customStatus = document.getElementById("custom-status");
+
 const charData = JSON.parse(localStorage.getItem("data")) || [];
 const charCopyList = document.getElementById("copy-list");
 const currentChar = {};
@@ -81,6 +89,7 @@ const updateCharContainer = () => {
                 <option value="Stunned">Stunned</option>
                 <option value="Unconscious">Unconscious</option>
                 <option value="Concentration">Concentration</option>
+                <option value="Custom">Custom</option>
             </select>
             <button class="status-btn" onclick="select(this)" type="button">Add Status</button>
             <div class="status-list"></div>
@@ -106,10 +115,8 @@ const updateCharContainer = () => {
     if(isNumeric(damage)){
         charData[dataArrIndex].damage = damEl.value;
         localStorage.setItem("data", JSON.stringify(charData));
-        update();
     }
 }
-
 
 const deleteChar = (buttonEl) => {
     const dataArrIndex = charData.findIndex(
@@ -118,8 +125,8 @@ const deleteChar = (buttonEl) => {
 
     buttonEl.parentElement.remove();
     charData.splice(dataArrIndex, 1);
-    update();
     localStorage.setItem("data", JSON.stringify(charData));
+    update();
 }
 
 const deleteStatus = (statEl) => {
@@ -136,27 +143,88 @@ const deleteStatus = (statEl) => {
     charData[dataArrIndex].status.splice(statusIndex, 1);
     statEl.remove();
     localStorage.setItem("data", JSON.stringify(charData));
+}
+
+const addConcentration = (parentEl, parentID)  => {
+    const cardText = "Concentration: "+concentrationSpell.value;
+
+    const dataArrIndex = charData.findIndex(
+        (char) => char.id === parentID
+    );
+    if(!charData[dataArrIndex].status.includes(cardText)){
+        charData[dataArrIndex].status.push(cardText)
+        parentEl.children[2].innerHTML += `<div onclick="deleteStatus(this)" class="status-card">${cardText}</div>`;
+        localStorage.setItem("data", JSON.stringify(charData));
+    }
+    concentrationForm.style.display = "none";
+    update();
+}
+
+
+const addCustom = (parentEl, parentID)  => {
+    const value = customStatus.value;
+
+    const dataArrIndex = charData.findIndex(
+        (char) => char.id === parentID
+    );
+
+    if(!charData[dataArrIndex].status.includes(value)){
+        charData[dataArrIndex].status.push(value)
+        parentEl.children[2].innerHTML += `<div onclick="deleteStatus(this)" class="status-card">${value}</div>`;
+        localStorage.setItem("data", JSON.stringify(charData));
+    }
+    customForm.style.display = "none";
     update();
 }
 
 const select = (buttonEl) => {
     const selectedStatus = buttonEl.parentElement.children[0].value;
-    const dataArrIndex = charData.findIndex(
-        (char) => char.id === buttonEl.parentElement.parentElement.id
-    );
+    const parentEl = buttonEl.parentElement;
+    const parentID = buttonEl.parentElement.parentElement.id;
 
-    if(selectedStatus && !charData[dataArrIndex].status.includes(selectedStatus)){
-        charData[dataArrIndex].status.push(selectedStatus)
-        buttonEl.parentElement.children[2].innerHTML += `<div onclick="deleteStatus(this)" class="status-card">${selectedStatus}</div>`;
+    switch (selectedStatus) {
+        case "Concentration":
+            concentrationSpell.value = "";
+            concentrationForm.addEventListener("submit", (e) => {
+                e.preventDefault();
+                addConcentration(parentEl, parentID);
+            });
+            concentrationForm.style.display = "flex";
+            
+            concentrationForm.removeEventListener("submit", (e) => {
+                e.preventDefault();
+                addConcentration(parentEl, parentID);
+            });
+            break;
+        case "Custom":
+            customStatus.value = "";
+            customForm.addEventListener("submit", (e) => {
+                e.preventDefault();
+                addCustom(parentEl, parentID);
+            });
+            customForm.style.display = "flex";
+            break;
+        case null:
+            break;
+        case "":
+            break;
+        default:
+            const dataArrIndex = charData.findIndex(
+                (char) => char.id === parentID
+            );
+            if(!charData[dataArrIndex].status.includes(selectedStatus)){
+                charData[dataArrIndex].status.push(selectedStatus)
+                buttonEl.parentElement.children[2].innerHTML += `<div onclick="deleteStatus(this)" class="status-card">${selectedStatus}</div>`;
+                localStorage.setItem("data", JSON.stringify(charData));
+            }
+            break;
     }
-    localStorage.setItem("data", JSON.stringify(charData));
 }
   
 
 const reset = () => {
     charName.value = "";
     charRoll.value = "";
-    currentTask = {};
 }
 
 
@@ -179,6 +247,17 @@ clearBtn.addEventListener("click", () => {
     localStorage.setItem("data", JSON.stringify(charData));
     update();
     reset()
+});
+
+
+closeCustom.addEventListener("click", () => {
+    customStatus.value = "";
+    customForm.style.display = "none";
+});
+
+closeSpell.addEventListener("click", () => {
+    concentrationSpell.value = "";
+    concentrationForm.style.display = "none";
 });
 
 const update = () =>{
